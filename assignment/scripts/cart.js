5973ef7deb3da58702f5ae6a250474f53367098f
Evan Bouman <evan.bouman@gmail.com>
2024-08-25 12:58:39 -0500

    Created functions for the first part of the assignment (addItem, listItems, empty)

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,14 +2,38 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+let basket = [];
+
+function addItem(item) {
+    basket.push(item);
+    return true;
+}
 
 
+console.log(basket);
+console.log(`Adding Pasta (expect true)`, addItem(`Pasta`));
+console.log(`Adding Marinara Sauce (expect true)`, addItem(`Marinara Sauce`));
+console.log(`Adding Parmesan (expect true)`, addItem(`Parmesan`));
+console.log(`Basket is now: `, basket);
 
 
+function listItems() {
+    for (i = 0; i < basket.length; i++) {
+        console.log(basket[i]);
+    }
+}
+
+listItems();
 
 
+function empty() {
+    while (basket.length>0){
+        basket.pop();
+    }
+}
 
-
+empty();
+console.log(`Basket should be empty`, empty(), basket);
 
 
 // DO NOT MODIFY
